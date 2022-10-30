dbe32bc61033a766afe96a603776d4991cc4517a
Vitaliy Pavlovskiy <sledozar@gmail.com>
2022-10-31 01:12:44 +0200

    finish-hw-05

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { auth: ctrl } = require("../../contollers");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 // signup
@@ -21,4 +21,12 @@ router.post(
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
+
 module.exports = router;
